36e5d9b9508c7692ce339157a3a043941f7d06ad
kimseoyeon <flowersayo0811@ewhain.net>
2021-10-09 01:14:02 +0900

    [10월 9일] js파일 분리 및 모든페이지에 토글버튼 추가

---
+++ colors.js
@@ -0,0 +1,41 @@
+var Link ={
+    setColor :function(color){
+    
+    var alist=document.querySelectorAll('a')
+    var i=0;
+
+    while (i<alist.length){
+          alist[i].style.color=color;
+          i+=1;
+    }
+   }
+}
+
+var Body={
+setColor :function (color){
+    document.querySelector('body').style.color= color;
+},
+setBackGroundColor:function (color){
+    document.querySelector('body').style.backgroundColor= color;
+}
+}
+
+
+function nightDayHandler(self){
+ var target =document.querySelector('body');
+
+if (self.value==='night'){
+     Body.setBackGroundColor('black');
+     Body.setColor('white');
+     self.value='day';
+     Link.setColor('powderblue');
+}
+else{
+
+    Body.setBackGroundColor('white');
+    Body.setColor('black');
+    self.value='night';
+    Link.setColor('blue');
+
+}
+}
